49d8b2b642a932f9d61d46b484c4104e893d05f5
Pedro Donda <phfdonda@gmail.com>
2020-06-17 13:23:35 -0300

    linter

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,6 @@
 class Member < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
   scope :member_list, -> { all.order('created_at DESC') }
 
   devise :database_authenticatable, :registerable,
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-
   scope :post_list, -> { all.order('created_at DESC') }
 
   belongs_to :member
